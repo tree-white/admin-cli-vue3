3a660f8382de11355f07d13dc376612aa9068a38
徐泳楷 <treewhite@yeah.net>
2022-07-02 21:45:57 +0800

    fix：去掉markdown的cdn改为包引入

--- src/components/Markdown/Editor.ts
+++ src/components/Markdown/Editor.ts
@@ -1,18 +1,18 @@
 import { uploadImage } from '@/apis/uploadApi'
-
+import Editor from '@toast-ui/editor'
 export default class {
   // 定义编辑器实例
-  editor: toastui.Editor
+  editor: Editor
   isFullscreen: boolean
   constructor(el: string, initialValue: string, height: string, placeholder: string = '请输入内容...') {
-    this.editor = new toastui.Editor({
-      el: document.querySelector(el),
+    this.editor = new Editor({
+      el: document.querySelector(el)!,
       initialEditType: 'markdown',
       previewStyle: 'vertical',
       height,
       initialValue,
       placeholder,
-      toolbarItems: this.toolbar()
+      toolbarItems: this.toolbar() as []
     })
 
     this.isFullscreen = false
